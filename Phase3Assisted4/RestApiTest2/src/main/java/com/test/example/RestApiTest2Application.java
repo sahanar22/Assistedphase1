5a8514d0b56aaadc6067abe29f51a04e667981c3
sahanar22 <97723197+sahanar22@users.noreply.github.com>
2022-03-07 10:09:12 +0530

    Add files via upload

---
+++ Phase3Assisted4/RestApiTest2/src/main/java/com/test/example/RestApiTest2Application.java
@@ -0,0 +1,13 @@
+package com.test.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiTest2Application.class, args);
+	}
+
+}
